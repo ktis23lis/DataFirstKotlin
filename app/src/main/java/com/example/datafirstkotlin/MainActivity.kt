124ec8988a8d4ab06a9bc44a5bccce30f2d45841
kris23lis <kris23lis@gmail.com>
2021-05-18 14:35:20 +0300

    first kotlin

--- app/src/main/java/com/example/datafirstkotlin/MainActivity.kt
+++ app/src/main/java/com/example/datafirstkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.datafirstkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import java.util.*
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
      lateinit var tv2 : TextView
      val person = Person("Human ", "Humanov")
      var person2 = person.copy()
+    lateinit  var view : View
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
